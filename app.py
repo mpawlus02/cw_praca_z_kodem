1986ab7a3ae1e9fbb016625bffdda6913140fba0
mpawlus02 <133579707+mpawlus02@users.noreply.github.com>
2023-05-14 16:43:31 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
-"""
-asd
-"""
+
 app = Flask(__name__)
 @app.route('/')
 def index():
+"""
+asd
+"""
     return '<h1>Hello WSB! Greetings from Flask!</h1>'
 if __name__ == "__main__":
     app.run(debug=True)
